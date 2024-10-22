3bcae3bfde923f824464ea6d9772411a90365bda
JapaneseZgredek <fabian.zbranski@gmail.com>
2024-10-22 19:13:22 +0200

    Make basic welcome page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+import routers.home
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.on_event("startup")
 async def startup():
     print("Start up called")
+
+app.include_router(routers.home.router)
